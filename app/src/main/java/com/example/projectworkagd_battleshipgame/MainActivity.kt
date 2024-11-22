6ebc927f2a6e4c16e021dc690398d49c827c6308
viktorahnstrom <167093877+viktorahnstrom@users.noreply.github.com>
2024-11-22 12:23:56 +0100

    Test
    - test

--- app/src/main/java/com/example/projectworkagd_battleshipgame/MainActivity.kt
+++ app/src/main/java/com/example/projectworkagd_battleshipgame/MainActivity.kt
@@ -21,7 +21,7 @@ class MainActivity : ComponentActivity() {
             ProjectworkAGDbattleshipGameTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
                     Greeting(
-                        name = "Android",
+                        name = "Android!",
                         modifier = Modifier.padding(innerPadding)
                     )
                 }
