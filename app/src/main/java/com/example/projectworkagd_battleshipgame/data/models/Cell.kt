f39892f62b7bb26a910299f58f66b389664c9d35
Viktor-CVJC <Viktor.Carlsson99@Gmail.com>
2024-11-22 12:59:02 +0100

    Added Ship and Cell model
    Implemented ship and cell model.

---
+++ app/src/main/java/com/example/projectworkagd_battleshipgame/data/models/Cell.kt
@@ -0,0 +1,6 @@
+package com.example.projectworkagd_battleshipgame.data.models
+
+data class Cell(
+    var shipId: String? = null,
+    var isHit: Boolean = false
+)
---
+++ app/src/main/java/com/example/projectworkagd_battleshipgame/data/models/Ship.kt
@@ -0,0 +1,8 @@
+package com.example.projectworkagd_battleshipgame.data.models
+
+data class Ship(
+    val id: String = java.util.UUID.randomUUID().toString(),
+    val length: Int,
+    var isPlaced: Boolean = false,
+    var isVertical: Boolean = false
+)
