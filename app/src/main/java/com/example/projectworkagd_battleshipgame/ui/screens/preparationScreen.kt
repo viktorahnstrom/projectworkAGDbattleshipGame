6468b409ec3f8791d35a1b162223c11633b6a571
Viktor-CVJC <Viktor.Carlsson99@Gmail.com>
2024-11-25 09:11:06 +0100

    Created preparationScreen
    -Created preparationScreen and started implementing composable.

---
+++ app/src/main/java/com/example/projectworkagd_battleshipgame/ui/screens/preparationScreen.kt
@@ -0,0 +1,12 @@
+package com.example.projectworkagd_battleshipgame.ui.screens
+
+import androidx.compose.runtime.Composable
+import com.example.projectworkagd_battleshipgame.ui.viewmodels.PreparationViewModel
+
+@Composable
+fun PreparationScreen(
+    navController: NavController,
+    viewModel: PreparationViewModel = viewModel()
+) {
+
+}
