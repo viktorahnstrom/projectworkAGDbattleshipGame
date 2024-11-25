ee41f431ee23aa485e991dbbf85af2369b4de4aa
Viktor-CVJC <Viktor.Carlsson99@Gmail.com>
2024-11-25 10:34:03 +0100

    Fixed bugs in firebaseService.kt
    Fixed unresolved refrences

--- app/src/main/java/com/example/projectworkagd_battleshipgame/data/remote/FirebaseService.kt
+++ app/src/main/java/com/example/projectworkagd_battleshipgame/data/remote/FirebaseService.kt
@@ -1,15 +1,15 @@
 package com.example.projectworkagd_battleshipgame.data.remote
 
 import android.util.Log
+import com.example.projectworkagd_battleshipgame.data.models.Game
 import com.example.projectworkagd_battleshipgame.data.models.Player
-import com.google.android.gms.games.Game
 import com.google.firebase.Firebase
 import com.google.firebase.auth.auth
 import com.google.firebase.database.DataSnapshot
 import com.google.firebase.database.DatabaseError
 import com.google.firebase.database.ValueEventListener
 import com.google.firebase.database.database
-import com.google.firebase.database.getValue
+
 
 class FirebaseService {
     private val database = Firebase.database.reference
@@ -22,13 +22,10 @@ class FirebaseService {
     fun observePlayers(onPlayersUpdate: (List<Player>) -> Unit) {
         database.child("players").addValueEventListener(object : ValueEventListener {
             override fun onDataChange(snapshot: DataSnapshot) {
-                val playerList = mutableListOf<Player>()
-                for (playerSnapshot in snapshot.children) {
-                    playerSnapshot.getValue<Player>()?.let {
-                        playersList.add(it)
-                    }
+                val players = snapshot.children.mapNotNull {
+                    it.getValue(Player::class.java)
                 }
-                onPlayersUpdate(playersList)
+                onPlayersUpdate(players)
             }
 
             override fun onCancelled(error: DatabaseError) {
@@ -45,11 +42,12 @@ class FirebaseService {
         database.child("games").child(game.id).setValue(game)
     }
 
-    fun observeGame(gameId: String, onUpdate: (game) -> Unit) {
+    fun observeGame(gameId: String, onUpdate: (Game) -> Unit) {
         database.child("games").child(gameId)
             .addValueEventListener(object : ValueEventListener {
                 override fun onDataChange(snapshot: DataSnapshot) {
-                    snapshot.getValue<Game>()?.let { onUpdate(it) }
+                    val game = snapshot.getValue(Game::class.java)
+                    game?.let { onUpdate(it) }
                 }
 
                 override fun onCancelled(error: DatabaseError) {
