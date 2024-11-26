aae23df150cb12474f10c4c273f2bc431cc69472
Viktor-CVJC <Viktor.Carlsson99@Gmail.com>
2024-11-26 11:24:10 +0100

    Created ChallengeDialog.kt
    - implemented ChallengeDialog component for multiplayer.
    - 30 second timer
     - different states for challengers and recievers. Features accept/decline/cancel actions.
    - auto decline timeout
    - Challenge dialog ui

---
+++ app/src/main/java/com/example/projectworkagd_battleshipgame/ui/components/ChallengeDialog.kt
@@ -0,0 +1,58 @@
+package com.example.projectworkagd_battleshipgame.ui.components
+
+import androidx.compose.material3.AlertDialog
+import androidx.compose.material3.Button
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableIntStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import kotlinx.coroutines.delay
+
+@Composable
+fun ChallengeDialog(
+    challengerName: String,
+    isSender: Boolean,
+    onAccept: () -> Unit,
+    onDecline: () -> Unit,
+    onDismiss: () -> Unit
+) {
+    var timeLeft by remember { mutableIntStateOf(30) }
+
+    LaunchedEffect(key1 = true) {
+        while (timeLeft > 0) {
+            delay(1000)
+            timeLeft--
+        }
+        if (timeLeft == 0) {
+            onDecline()
+        }
+    }
+
+    AlertDialog(
+        onDismissRequest = onDismiss,
+        title = { Text(if (isSender) "Waiting for response..." else "New Challenge!") },
+        text = {
+            Text(
+                if (isSender)
+                    "Waiting for $challengerName  to respond \nTime left: $timeLeft seconds"
+                else
+                    "$challengerName has challenged you!\nTime left: $timeLeft seconds"
+            )
+        },
+        confirmButton = {
+            if (!isSender) {
+                Button(onClick = onAccept) {
+                    Text("Accept")
+                }
+            }
+        },
+        dismissButton = {
+            Button(onClick = onDecline) {
+                Text(if (isSender) "Cancel" else "Decline")
+            }
+        }
+    )
+}
