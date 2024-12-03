c3b0ee903c01b420720d172a19c553f96f96b5fa
Viktor-CVJC <Viktor.Carlsson99@Gmail.com>
2024-12-03 14:44:51 +0100

    Change game data models
    -updated game model to use string based boards
    - added board state conversion
    - expandend gamestate model with more player tracking
    - removed vertical orientation from ship model to simplify it
    - added further player ready state tracking

--- app/src/main/java/com/example/projectworkagd_battleshipgame/data/models/Game.kt
+++ app/src/main/java/com/example/projectworkagd_battleshipgame/data/models/Game.kt
@@ -14,13 +14,39 @@ data class Game(
     val player2Ready: Boolean = false,
     val status: GameStatus = GameStatus.SETUP,
     var currentTurn: String = "",
-    val board1: Board? = null,
-    val board2: Board? = null,
-    val winner: String? = null
+    val winner: String? = null,
+    val board1String: String? = null,
+    val board2String: String? = null
 ) {
     init {
         if (currentTurn.isEmpty()) {
             currentTurn = player1Id
         }
     }
+
+    val board1: List<List<Board.CellState>>?
+        get() = board1String?.split(",")?.map { row ->
+            row.map { cell ->
+                when (cell) {
+                    'E' -> Board.CellState.EMPTY
+                    'S' -> Board.CellState.SHIP
+                    'H' -> Board.CellState.HIT
+                    'M' -> Board.CellState.MISS
+                    else -> Board.CellState.EMPTY
+                }
+            }
+        }
+
+    val board2: List<List<Board.CellState>>?
+        get() = board2String?.split(",")?.map { row ->
+            row.map { cell ->
+                when (cell) {
+                    'E' -> Board.CellState.EMPTY
+                    'S' -> Board.CellState.SHIP
+                    'H' -> Board.CellState.HIT
+                    'M' -> Board.CellState.MISS
+                    else -> Board.CellState.EMPTY
+                }
+            }
+        }
 }
--- app/src/main/java/com/example/projectworkagd_battleshipgame/data/models/GameState.kt
+++ app/src/main/java/com/example/projectworkagd_battleshipgame/data/models/GameState.kt
@@ -1,8 +1,13 @@
 package com.example.projectworkagd_battleshipgame.data.models
 
 data class GameState(
-    val isLoading: Boolean = false,
+    val isLoading: Boolean = true,
     val status: GameStatus = GameStatus.SETUP,
-    val currentPlayerId: String? = null,
-    val error: String? = null
+    val currentPlayerId: String = "",
+    val player1Id: String = "",
+    val player2Id: String = "",
+    val player1Ready: Boolean = false,
+    val player2Ready: Boolean = false,
+    val error: String? = null,
+    val winner: String? = null
 )
